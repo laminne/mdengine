8737cbbc5db80b9d4ff3878148b11d63c49ed6c1
laminne tatsuto yamamoto <laminne33569@approvers.dev>
2020-12-31 14:20:46 +0900

    Verison Up

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # mdengine
+
+MarkDown形式のファイルをHTMLに変換&簡単なサイトを作成するためのRubyプログラム群です  
+
+## How To Use
+### 必要なもの
+- Ruby 2.7+ (x86_64)
+
+`ruby ./main.rb help`でヘルプを表示します
+
+```
+ruby main.rb [argment] [filename]
+
+--build [filename] --生HTMLを生成
+--build-all [生成後のファイル名] --buildで生成された中間ファイルに
+スタイルを適用して./public以下に生成
+-v バージョン表示
+```
--- main.rb
+++ main.rb
@@ -9,29 +9,29 @@ if ARGV.count > 3
   exit(1)
 end
 
-if ARGV[0] == "build" && ARGV[1] != nil
+if ARGV[0] == "--build" && ARGV[1] != nil
   p "[info] ビルドを開始します"
   $filename = ARGV[1]
   Make.build
   p "[log] ./md.html を中間ファイルとして生成しました\n このファイルは変更しないでください"
 end
 
-if ARGV[0] == "build-all" && ARGV[1] != nil
+if ARGV[0] == "--build-all" && ARGV[1] != nil
   $filename = ARGV[1]
   p "[info] 中間ファイルからページを生成します\nビルド終了後に./publish/#{$filename}.html が生成されます"
   Make.publish
   p "[info] ビルドが終了しました"
 end
 
-if ARGV[0] == "help"
+if ARGV[0] == "--help" || ARGV[0] == "-h"
   p "-----MarkDown-HTML Generator-----"
   p "ruby main.rb [argments] [filename]"
-  p "build [MDのファイル名]  スタイル適用前の生HTMLを生成"
-  p "build-all [生成後のファイル名]  スタイルを適用したページを生成"
+  p "--build [MDのファイル名]  スタイル適用前の生HTMLを生成"
+  p "--build-all [生成後のファイル名]  スタイルを適用したページを生成"
   p "-v バージョンを表示"
 end
 
 if ARGV[0] == "-v"
-  p "MD-HTML Generator Ver. 1.0.0"
-  p "2020/12/28"
+  p "MD-HTML Generator Ver. 1.0.2"
+  p "2020/12/31"
 end
